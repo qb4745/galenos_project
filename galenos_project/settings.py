15e26dd302d63c6425467cb9b71797d1e128720d
Jaime Vicencio <jai.vicencio@duocuc.cl>
2023-11-13 13:13:27 -0300

    final

--- galenos_project/settings.py
+++ galenos_project/settings.py
@@ -79,9 +79,16 @@ WSGI_APPLICATION = "galenos_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "api_rest_django",
+        "USER": "mysql",
+        "PASSWORD": "password",
+        "HOST": "mysql_db",
+        "PORT": "3306",
+        "OPTIONS": {
+            "sql_mode": "traditional",
+        },
+    },
 }
 
 
